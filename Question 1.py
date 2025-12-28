e34c18100679e5f9c1c6ef04da9e651246f270e6
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:06:53 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,16 @@
+'''
+Describe Number Based on Divisibility
+Write a function describe_number that takes an integer as input and returns:
+
+"Fizz" if the number is divisible by 3.
+"Buzz" if divisible by 5.
+"FizzBuzz" if divisible by both 3 and 5.
+"Normal" if it is divisible by neither.
+NOTE: This is a function type question, you don't have to take input or print the output, just have to complete the required function definition.
+
+Example
+describe_number(9) -> "Fizz"
+describe_number(10) -> "Buzz"
+describe_number(15) -> "FizzBuzz"
+describe_number(7) -> "Normal"
+'''
