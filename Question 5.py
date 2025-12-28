5a41bc788131e449f732872e1d674d4dbf0c0ba1
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 20:10:26 +0530

    Create Question 5.py

---
+++ Question 5.py
@@ -0,0 +1,29 @@
+'''
+Replace Consonants with Hash
+Write a program that takes a multi-line string input and returns a new string where all consonants are replaced by #, but vowels stay unchanged.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+The first line contains an integer n, the number of lines.
+The next n lines contain the passage.
+Output Format
+
+Print the passage with consonants replaced by # and vowels unchanged.
+Hint
+Use the isalpha() method to check for alphabets and string comparisons to identify vowels.
+
+Example
+
+Input
+
+2
+Hello World
+Good Night
+
+Output
+
+#e##o #o###
+#oo# #i###
+'''
